3cb8c887ddd78d87a7f1f6094f13fef97e5d4da1
dung997bn <phaichisophan@gmail.com>
2021-01-12 20:35:50 +0700

    End of lesson 149

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,13 +7,32 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
-  { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
-  { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
-  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
+  {
+    path: 'test-error',
+    component: TestErrorComponent,
+    data: { breadcrumb: 'Test Errors' },
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not Found' },
+  },
   {
     path: 'shop',
     loadChildren: () =>
-      import('./shop/shop.module').then((mod) => mod.ShopModule), data: { breadcrumb: 'Shop' }
+      import('./shop/shop.module').then((mod) => mod.ShopModule),
+    data: { breadcrumb: 'Shop' },
+  },
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./basket/basket.module').then((mod) => mod.BasketModule),
+    data: { breadcrumb: 'Basket' },
   },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
@@ -22,4 +41,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { BasketModule } from './basket/basket.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,6 +22,7 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
     CoreModule,
     ShopModule,
     HomeModule,
+    BasketModule,
     NgxSpinnerModule
   ],
   providers: [
